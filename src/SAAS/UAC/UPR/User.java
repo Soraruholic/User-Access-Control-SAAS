81b502ab7c3687734fa5776ad16912eeeb143170
ouershua <1666403795@qq.com>
2023-10-25 15:14:55 +0800

    把登录模块和原来的模块进行了整合： 1.合并user类，对原来的User增加了成员和函数。 2.对loginPage类进行修改：去除身份证验证；忘记密码功能完全由覆写实现 3.todo：其余模块可能由于User类的修改导致构造函数需要修改-待完成；单向加密：加盐或者换方式

--- src/SAAS/UAC/UPR/User.java
+++ src/SAAS/UAC/UPR/User.java
@@ -1,7 +1,6 @@
 package SAAS.UAC.UPR;
-import java.util.ArrayList;
+
 import java.util.HashSet;
-import java.util.List;
 
 // User is an abstract class that represents a user in the system.
 public abstract class User {
@@ -11,14 +10,60 @@ public abstract class User {
     protected HashSet<Role> roleList;
     protected HashSet<Permission> permissionPool;
     protected HashSet<Role> rolePool;
-    public User(String name, String userID, HashSet<Permission> permissionList, HashSet<Role> roleList, HashSet<Permission> permissionPool, HashSet<Role> rolePool) {
+
+    protected String passwordMd5;
+
+    protected String phonenumber;
+
+
+    public User(String name, String userID , HashSet<Permission> permissionList , HashSet<Role> roleList, HashSet<Permission> permissionPool, HashSet<Role> rolePool, String passwordMd5, String phonenumber) {
         this.name = name;
         this.userID = userID;
         this.permissionList = permissionList;
         this.roleList = roleList;
         this.permissionPool = permissionPool;
         this.rolePool = rolePool;
+        this.phonenumber = phonenumber;
+        this.passwordMd5 = passwordMd5;
+    }
+
+
+    public String getUsername() {
+        return name;
+    }
+
+
+    public void setUsername(String username) {
+        this.name = username;
+    }
+
+
+    public String getPassword() {
+        return passwordMd5;
+    }
+
+
+    public void setPassword(String password) {
+        this.passwordMd5 = password;
+    }
+
+
+//    public void setPersonID(String personID) {
+//        this.personID = personID;
+//    }
+
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
     }
 }
+
+
+
 
 
